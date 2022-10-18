ae62b44253f85abf67ae920247f4f634691b34b0
cantordust <43831101+cantordust@users.noreply.github.com>
2022-10-18 11:42:09 +0200

    ==[ Minor updates ]==

---
+++ pyrception/conf.py
@@ -0,0 +1,31 @@
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
+import sys
+
+# --------------------------------------
+from decouple import config
+
+# --------------------------------------
+from loguru import logger
+
+# * Log format * #
+LOG_FORMAT = config(
+    "PYRCEPTION_LOG_FORMAT",
+    # default="==[ <green>{time:DD-MMM-YYYY@HH:mm:ss}</green> ] <level>{level:<8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    default="==[ <green>{time:DD-MMM-YYYY@HH:mm:ss}</green> ] <level>{level:<8}</level> | <level>{message}</level>",
+)
+LOG_CONFIG = {
+    "handlers": [
+        {
+            "sink": sys.stdout,
+            "format": LOG_FORMAT,
+        },
+        {
+            "sink": sys.stderr,
+            "format": LOG_FORMAT,
+        },
+    ]
+}
+
+logger.configure(**LOG_CONFIG)
