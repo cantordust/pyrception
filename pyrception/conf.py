96e9203baac288c5486e1dbf2a09d2a4e035cc93
cantordust <>
2024-03-14 18:03:43 +0100

    - Updated environment and configuration

--- pyrception/conf.py
+++ pyrception/conf.py
@@ -6,19 +6,43 @@ from decouple import config
 # --------------------------------------
 from loguru import logger
 
-LOG_FORMAT = config(
+# --------------------------------------
+import torch as pt
+
+# Logger configuration
+# ==================================================
+log_format = config(
     "PYRCEPTION_LOG_FORMAT",
-    # default="==[ <green>{time:DD-MMM-YYYY@HH:mm:ss}</green> ] <level>{level:<8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-    # default="==[ <green>{time:DD-MMM-YYYY@HH:mm:ss}</green> ] <level>{level:<8}</level> | <level>{message}</level>",
-    default="==[ <green>{time:DD-MMM-YYYY@HH:mm:ss}</green> | <level>{message}</level>",
+    default="Pyrception | <green>{time:DD-MMM-YYYY@HH:mm:ss}</green> | <level>{message}</level>",
 )
-LOG_CONFIG = {
+log_config = {
     "handlers": [
         {
             "sink": sys.stdout,
-            "format": LOG_FORMAT,
+            "format": log_format,
         }
     ]
 }
 
-logger.configure(**LOG_CONFIG)
+logger.configure(**log_config)
+logger.info("Logger configured.")
+
+# PyTorch configuration
+# ==================================================
+# Default tensor type
+pt.set_default_dtype(pt.float16)
+
+# Default device
+device = config("PYRCEPTION_PYTORCH_DEVICE", default="cpu")
+
+if device.split(":")[0] == "cuda":
+    if pt.cuda.is_available():
+        device = pt.device(device)
+    else:
+        logger.warning(f"CUDA is unavailable, using 'cpu' as a fallback.")
+        device = pt.device("cpu")
+
+else:
+    device = pt.device("cpu")
+
+logger.info(f"Default PyTorch device: '{device}'.")
